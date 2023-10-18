0308ef2b92600527ca88d7b88a7543074c559e5c
hashith <dasunbro1995@gmail.com>
2023-10-18 11:05:39 +0530

    edited front page

--- pages/Landing/landing.jsx
+++ pages/Landing/landing.jsx
@@ -10,8 +10,8 @@ function Landing() {
         <Navbar/>
         <div className='main-section'>
           <div className='semi-section'>
-            <h1 className='heading'>Say Hello to Your <br></br>Fitness Partner</h1>
-            <p className='text'>Healthy app give track your health status and give you<br></br> helaths suggetions according to you</p>
+            <h1 className='heading'>Say Hello to Your <br></br>AI Fitness Partner</h1>
+            <p className='text'>Healthy app give track your health status and give you<br></br> helaths suggetions according to you with AI suggestions</p>
             <Link className='btn btn-primary ms-5' to="/login">Login</Link>
             <Link className='btn btn-primary ms-2' to="/signup">Sign Up</Link>
           </div>
